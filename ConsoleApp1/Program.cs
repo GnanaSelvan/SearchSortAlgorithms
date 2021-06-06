56a2fc4fa46813c6dd3c64c2933acfa773c808ab
GnanaSelvan <gnanaselvan28@gmail.com>
2021-06-07 00:53:38 +0530

    Code push

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] a1 = { 1, 3, 4, 6, 5, 7, 4, 8 };
+            bool swapped = true;
+          
+
+            while(swapped)
+            {
+                swapped = false;
+                for (int i = 0; i < 7; i++)
+                {
+                    
+                    if (a1[i] > a1[i + 1])
+                    {
+                        int temp = a1[i];
+                        a1[i] = a1[i + 1];
+                        a1[i + 1] = temp;
+                        swapped = true;
+                    }
+                    
+                }
+                
+                
+            }
+    }
+}
---
+++ SearchSortAlgorithms/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SearchSortAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] unsortedarray = { 1, 3, 4, 7, 5, 4, 8 };
+            int i;
+            int temp;
+            bool swapdone = true;
+
+            while (swapdone)
+            {
+                swapdone = false;
+                for (i = 0; i < unsortedarray.Length - 1; i++)
+                {
+                    //Console.WriteLine("unsortedarray[i]" + unsortedarray[i]);
+                    //Console.WriteLine("unsortedarray[i++]" + unsortedarray[i + 1]);
+                   
+                    if (unsortedarray[i] > unsortedarray[i + 1])
+                    {
+                        temp = unsortedarray[i];
+                        unsortedarray[i] = unsortedarray[i + 1];
+                        unsortedarray[i + 1] = temp;
+                        swapdone = true;
+                    }
+                }
+            }
+
+            Console.WriteLine("Sorted array");
+            foreach (int item in unsortedarray)
+            {             
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
